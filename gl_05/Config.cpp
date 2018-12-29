10e6798135403eb77824e8b4a1e15775568ba816
Radoslaw Komorowski <283680@pw.edu.pl>
2018-12-29 01:43:17 +0100

    fix small issue with window size

--- gl_05/Config.cpp
+++ gl_05/Config.cpp
@@ -3,7 +3,7 @@
 float Config::CAMERA_SENSITIVITY = 15.f;
 float Config::CAMERA_SPEED = 5.f;
 int Config::WINDOW_WIDTH = 800;
-int Config::CAMERA_SPEED = 600;
+int Config::WINDOW_HEIGHT = 600;
 
 Config::Config() {
 }
